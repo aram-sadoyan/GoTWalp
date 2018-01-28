484090d6c792e996ee87896cd06d43402b77d26a
aramksadoyan <aram.sadoyan@picsart.com>
2018-01-28 18:05:54 +0400

    Initial commit

---
+++ app/src/main/java/com/example/user/gotwalp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.user.gotwalp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	//1 Make a note of your new app ID. You'll need to add it to your app's source code to run AdMob.
+	//ca-app-pub-6630049462645854~6334922802
+
+	//Create an ad unit to display ads in your app.
+	//If your app is published to Google Play or the App Store, remember to come back to link your app.
+
+
+
+//	Banner	ca-app-pub-3940256099942544/6300978111
+//	Interstitial	ca-app-pub-3940256099942544/1033173712
+//	Rewarded Video	ca-app-pub-3940256099942544/5224354917
+//	Native Advanced	ca-app-pub-3940256099942544/2247696110
+//	Native Express	(Small template): ca-app-pub-3940256099942544/2793859312
+//			(Large template): ca-app-pub-3940256099942544/2177258514
+
+	private String testBannerAd = "ca-app-pub-3940256099942544/6300978111";
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+
+
+
+	}
+}
